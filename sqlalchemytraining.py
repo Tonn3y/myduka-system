422182e5aee265524dc32996ceb6e9f9ebfc3f19
Tonn3y <tonykibet42@gmail.com>
2024-09-26 15:59:54 +0300

    sqlalchemytraining.py

--- sqlalchemytraining.py
+++ sqlalchemytraining.py
@@ -16,6 +16,10 @@ Session = sessionmaker(bind = engine)
 session = Session()
 #  query all the employees
 employees = session.query(Employee).all()
+# Inserting a newuser
+newemployee1 = Employee(firstname = 'Tess',lastname = 'Wairimu',email = 'dontknowtess@gmail.com',city = 'Brussels')
+session.add(newemployee1)
+session.commit()
 #  loop through and print the employee
 for employee in employees :
     print(f"ID: {employee.employeeid}, firstname: {employee.firstname}, lastname: {employee.lastname}, Email:{employee.email},City:{employee.city}")
