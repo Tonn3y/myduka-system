9ce2494c2850c3d682533edeb3af4ac3a3ebc92c
Tonn3y <tonykibet42@gmail.com>
2025-03-13 20:46:47 +0300

    main1.py

--- main1.py
+++ main1.py
@@ -23,7 +23,7 @@ def login() :
 def user() :
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return f"<h1>{user}</h1>"    
     else :
         return redirect(url_for("login")) 
     
